61439f6a27da4ed2c3eb316e1bab80c732509a32
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-25 13:09:27 -0500

    add: ingredient jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,38 @@ const sampleRecipes = [
     servings: 3,
     cookTime: '1:45',
     instructions:
-      '1. Put salt on chicken \n2. Put chicken in oven \n3. Eat chicken'
+      '1. Put salt on chicken \n2. Put chicken in oven \n3. Eat chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 tbs'
+      }
+    ]
   },
   {
     id: 2,
     name: 'Plain Pork',
     servings: 5,
     cookTime: '0:45',
-    instructions: '1. Put parpika on pork \n2. Put pork in oven \n3. Eat pork'
+    instructions: '1. Put parpika on pork \n2. Put pork in oven \n3. Eat pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 pounds'
+      },
+      {
+        id: 2,
+        name: 'paprika',
+        amount: '2 tbs'
+      }
+    ]
   }
 ];
 
